cb1319a20bf71f57685e4ed22f14230bac81579c
Matt Watson <mattwat@gmail.com>
2019-07-30 22:32:12 -0400

    fix site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `FABRIC`,
+    description: `FABRIC is Adaptive programmaBle Research Infrastructure for Computer science`,
+    author: `mbwatson`,
   },
   plugins: [
     `gatsby-plugin-sass`,
