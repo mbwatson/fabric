661bf6f2c48530365a305d1829ebaa777e57160f
Matt Watson <mattwat@gmail.com>
2019-08-01 15:37:22 -0400

    add functionality for partner logo grouping
    Squashed commit of the following:
    
    commit 651f1715c4b969abdbe98de5847b7149d325abea
    Author: Matt Watson <mattwat@gmail.com>
    Date:   Thu Aug 1 15:37:06 2019 -0400
    
        separate logo query by partner group
    
    commit d2222ea36bb1fdef16d71ee43ada57f1f5acc132
    Author: Matt Watson <mattwat@gmail.com>
    Date:   Thu Aug 1 15:36:52 2019 -0400
    
        group partner logos

--- src/sections/Partners.js
+++ src/sections/Partners.js
@@ -7,13 +7,35 @@ import { Paragraph } from '../components/Typography'
 
 const logosQuery = graphql`
     query {
-        allFile(filter: {relativeDirectory: {eq: "partners"}}) {
-            logos: edges {
+        logos: allFile(filter: {relativeDirectory: {eq: "partners"}}) {
+            edges {
                 node {
                     id
                     childImageSharp {
                         id
-                        fixed(fit: CONTAIN, height: 40) {
+                        fixed(fit: CONTAIN, height: 30) {
+                            base64
+                            tracedSVG
+                            aspectRatio
+                            width
+                            height
+                            src
+                            srcSet
+                            srcWebp
+                            srcSetWebp
+                            originalName
+                        }
+                    }
+                }
+            }
+        }
+        coreLogos: allFile(filter: {relativeDirectory: {eq: "partnersCore"}}) {
+            edges {
+                node {
+                    id
+                    childImageSharp {
+                        id
+                        fixed(fit: CONTAIN, height: 60) {
                             base64
                             tracedSVG
                             aspectRatio
@@ -32,12 +54,15 @@ const logosQuery = graphql`
     }
 `
 
-const partnerLogos = [
+const corePartnerLogos = [
     'renci.png',
     'uk.png',
     'clemson.png',
     'iit.png',
     'esnet.png',
+]
+
+const partnerLogos = [
     'columbia.png',
     'fiu.png',
     'gt.png',
@@ -54,7 +79,7 @@ const partnerLogos = [
 
 const ImageArrangement = styled.div`
     text-align: center;
-    margin: 2rem 0 6rem 0;
+    margin: 2rem 0;
 `
 
 const MutedImage = styled(Img)`
@@ -67,19 +92,34 @@ const MutedImage = styled(Img)`
     }
 `
 
+const CoreMutedImage = styled(MutedImage)`
+    margin: 1rem;
+`
+
 export const PartnersSection = props => {
     return (
         <StaticQuery
             query={ logosQuery }
             render={
                 data => {
-                    const sortedLogos = data.allFile.logos
+                    const sortedLogos = data.logos.edges
                         .sort((a, b) => partnerLogos.indexOf(a.node.childImageSharp.fixed.originalName) - partnerLogos.indexOf(b.node.childImageSharp.fixed.originalName))
+                    const sortedCoreLogos = data.coreLogos.edges
+                        .sort((a, b) => corePartnerLogos.indexOf(a.node.childImageSharp.fixed.originalName) - corePartnerLogos.indexOf(b.node.childImageSharp.fixed.originalName))
                     return (
                         <Section title="Partners">
                             <Paragraph>
                                 FABRIC is made possible by collaborations with the following organizations.
                             </Paragraph>
+                            <ImageArrangement>
+                                {
+                                    sortedCoreLogos.map(
+                                        ({ node: logo }) => (
+                                            <CoreMutedImage fixed={ logo.childImageSharp.fixed } />
+                                        )
+                                    )
+                                }
+                            </ImageArrangement>
                             <ImageArrangement>
                                 {
                                     sortedLogos.map(
