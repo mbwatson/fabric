e4a2b56487eee5deae3cf7ea75e36b3967f7028d
Matt Watson <mattwat@gmail.com>
2019-08-26 11:07:53 -0400

    style links in main content area

--- src/components/Layout/Main.js
+++ src/components/Layout/Main.js
@@ -8,4 +8,14 @@ export const Main = styled.main`
     background-color: transparent;
     position: relative;
     transition: transform 500ms;
+    a, a:visited {
+        transition: color 250ms, border-color 250ms;
+        color: var(--color-primary);
+        border-bottom: 1px solid;
+        border-color: var(--color-primary-light);
+    }
+    a:hover, a:active {
+        color: var(--color-primary-dark);
+        border-color: var(--color-primary-dark);
+    }
 `
