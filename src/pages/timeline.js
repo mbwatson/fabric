9339a385b14800ddba8cbd55cc12baa7fa190cba
Matt Watson <mattwat@gmail.com>
2019-09-03 14:06:37 -0400

    add initial styling for timeline view

--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -1,16 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
 import { FadeOnMount } from '../components/Anim'
 import { graphql } from 'gatsby'
 import { SEO } from '../components/SEO'
 import { Title, Heading, Paragraph } from '../components/Typography'
 
+const Timeline = styled.article`
+    padding-left: 2rem;
+`
+
+const TimelineNode = styled.article`
+    padding-left: 2rem;
+    position: relative;
+    &::before {
+        content: "";
+        position: absolute;
+        left: -1rem;
+        top: 0.5rem;
+        width: 1px;
+        height: 100%;
+        background-color: var(--color-primary);
+        border-radius: 50%;
+    }
+    &:last-child::before {
+        width: 0;
+    }
+    &::after {
+        content: "";
+        position: absolute;
+        left: -1.5rem;
+        top: 0.5rem;
+        width: 1rem;
+        height: 1rem;
+        background-color: var(--color-primary);
+        border-radius: 50%;
+    }
+`
+
 const TimelineItem = ({ date, title, description }) => {
     return (
-        <article style={{ marginBottom: '2rem' }}>
+        <TimelineNode>
             <Heading>{ date }</Heading>
             <h4>{ title }</h4>
             <Paragraph dangerouslySetInnerHTML={{ __html: description }} />
-        </article>
+        </TimelineNode>
     )
 }
 
@@ -22,17 +55,19 @@ const TimelinePage = ({ data }) => {
             <SEO title="Events" />
 
             <Title>FABRIC Development Timeline</Title>
-
-            {
-                allMarkdownRemark.edges.map(({ node }) => (
-                    <TimelineItem
-                        key={ node.frontmatter.title }
-                        date={ node.frontmatter.date }
-                        title={ node.frontmatter.title }
-                        description={ node.html }
-                    />
-                ))
-            }
+            
+            <Timeline>
+                {
+                    allMarkdownRemark.edges.map(({ node }) => (
+                        <TimelineItem
+                            key={ node.frontmatter.title }
+                            date={ node.frontmatter.date }
+                            title={ node.frontmatter.title }
+                            description={ node.html }
+                        />
+                    ))
+                }
+            </Timeline>
 
         </FadeOnMount>
     )
