1c628dcdf14173f11f9551cef82e7ba78fcbfc83
Matt Watson <mattwat@gmail.com>
2019-09-04 11:18:46 -0400

    increase header padding

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,10 +2,10 @@ import styled from 'styled-components'
 
 export const Header = styled.header`
     // & * {border: 1px solid #f99;}
-    background-color: var(--color-black);
+    background-color: var(--color-dark);
     padding: 0;
     display: flex;
     justify-content: center;
-    padding: 0.5rem;
+    padding: 3rem;
     z-index: 2;
 `
