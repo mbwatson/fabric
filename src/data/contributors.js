b43f069ed8e256f83d23e574a0880d77c525bb83
Matt Watson <mattwat@gmail.com>
2019-09-06 16:58:31 -0400

    remove LLC

--- src/data/contributors.js
+++ src/data/contributors.js
@@ -35,7 +35,7 @@ export const contributors = [
       {
         name: 'Tom Lehman',
         url: '#',
-        organization: 'Virnao LLC',
+        organization: 'Virnao',
          }
     ]
   },
