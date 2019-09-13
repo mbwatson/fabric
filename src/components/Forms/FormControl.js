d8153977ce71416192275d0cf2ca89a8680fb8bb
Matt Watson <mattwat@gmail.com>
2019-09-13 12:17:26 -0400

    remove form components

--- src/components/Forms/FormControl.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const FormControl = styled.div`
-    text-align: right;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    margin-bottom: 2rem;
-`
