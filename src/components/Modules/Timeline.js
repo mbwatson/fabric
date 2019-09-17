82b886caa5322212d9f2746c282770a933ca4b70
Matt Watson <mattwat@gmail.com>
2019-09-17 13:49:50 -0400

    reduce timeline tag text size
    reduce timeline tag text size
    
    fix translation offset on compact screen size

--- src/components/Modules/Timeline.js
+++ src/components/Modules/Timeline.js
@@ -7,7 +7,7 @@ import { Paragraph } from '../Typography'
 const TimelineTabs = styled.article`
     padding-left: 2rem;
     display: flex;
-    margin: 8rem 0 0 0;
+    margin: 6rem 0 0 0;
     display: flex;
     justify-content: center;
     width: 100%;
@@ -19,28 +19,30 @@ const TimelineTab = styled.div`
     align-items: center;
     transition: transform 250ms ease-out;
     transform-origin: 0% 50%;
-    transform: translateY(1.6rem) rotate(${ props => props.compact ? '-90deg' : '-40deg' });
+    transform: ${ props => props.compact ? 'translateY(1.6rem) rotate(-90deg)' : 'translateY(1.4rem) rotate(-40deg)' };
     cursor: pointer;
 `
 
 const Tag = styled.div`
     flex: 1;
-    height: 2.5rem;
-    padding: 0.5rem 0.75rem 0.5rem 1.75rem;
+    height: 2rem;
+    line-height: 1;
+    padding: 0.5rem 0.75rem 0.5rem 1.5rem;
     border-radius: 4px;
     border-width: 0 0.25rem 0 0: 
     border-style: solid;
     border-color: ${ props => props.active ? 'var(--color-secondary)' : 'var(--color-primary)' };
-    font-weight: bold;
+    // font-weight: bold;
+    font-size: 85%;
     white-space: nowrap;
     filter: drop-shadow(0 0 3px #00000022);
-    transform: ${ props => props.active ? 'translateX(1rem)' : 'translateX(2rem)' };
+    transform: ${ props => props.active ? 'translateX(0.5rem)' : 'translateX(1rem)' };
     ::selection { background: var(--color-primary); }
     ::-moz-selection { background: var(--color-primary); }
     transition: color 250ms, background-color 250ms, transform 250ms ease-out;
     color: ${ props => props.active ? 'var(--color-white)' : 'var(--color-primary)' };
     background-color: ${ props => props.active ? 'var(--color-primary)' : 'var(--color-primary-light)' };
-    clip-path: polygon(1.4rem 0%, 100% 0%, 100% 100%, 1.4rem 100%, 0% 50%);
+    clip-path: polygon(1.18rem 0%, 100% 0%, 100% 100%, 1.18rem 100%, 0% 50%);
     &::after { // guide
         content: "";
         left: -3rem;
@@ -51,7 +53,7 @@ const Tag = styled.div`
     }
     &:hover {
         color: ${ props => props.active ? 'var(--color-white)' : 'var(--color-primary-dark)' };
-        transform: ${ props => props.active ? 'translateX(1rem)' : 'translateX(1.85rem)' };
+        transform: ${ props => props.active ? 'translateX(0.75rem)' : 'translateX(1.15rem)' };
     }
 `
 
