a47490157f5e8c584c79ed66cbe7533f2f178053
Matt Watson <mattwat@gmail.com>
2019-09-16 23:40:16 -0400

    minimize horizontal rule component

--- src/components/HorizontalRule/HorizontalRule.js
+++ src/components/HorizontalRule/HorizontalRule.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 
 export const HorizontalRule = styled.hr`
     border: 0;
-    height: 0.5rem;
-    border-radius: 0.25rem;
-    background-color: var(--color-primary-light);
+    height: 1px;
+    background-color: var(--color-primary-dark);
 `
