1aab9a4c7c5c83a6cc9fcaf0e0a4ea9c906384ee
Matt Watson <mattwat@gmail.com>
2019-09-16 22:12:48 -0400

    add coming soon to docs viwq

--- src/pages/docs.js
+++ src/pages/docs.js
@@ -10,7 +10,7 @@ const DocsPage = () => (
         <Title>Documentation</Title>
 
         <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos enim aliquid, unde.
+            Coming soon.
         </Paragraph>
     </FadeOnMount>
 
