1cf700afa3f446857e67ae0ddd0a35da3110831f
Matt Watson <mattwat@gmail.com>
2019-10-01 16:48:54 -0400

    remove unused imports

--- src/pages/about/overview.js
+++ src/pages/about/overview.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import { graphql } from 'gatsby'
-import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 import { FadeOnMount } from '../../components/Anim'
 import { SEO } from '../../components/SEO'
