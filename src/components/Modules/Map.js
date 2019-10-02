2774bca9c88635e33f31459574678c56d601c3e2
Matt Watson <mattwat@gmail.com>
2019-10-02 10:07:26 -0400

    outline map data fetching

--- src/components/Modules/Map.js
+++ src/components/Modules/Map.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Module } from '../Layout'
 import fabricMapSvg from '../../images/fabric-map.svg'
 import {
@@ -9,9 +9,26 @@ import {
 } from 'react-simple-maps'
 
 export const MapModule = props => {
+    const [mapJson, setMapJson] = useState(null)
+
+    useEffect(() => {
+        const fetchMapJson = async url => {
+            fetch(url)
+                .then(response => {
+                    if (response.status !== 200) {
+                        console.log(response)
+                        setMapJson(response.json())
+                    } else {
+                        console.log('nothin')
+                    }
+                })
+        }
+        fetchMapJson('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+    }, [])
+    
     return (
         <Module title="Anticipated FABRIC Topology">
-            <img src={ fabricMapSvg } alt=""/>
+            <pre>{ JSON.stringify(mapJson, null, 2) }</pre>
         </Module>
     )
 }
