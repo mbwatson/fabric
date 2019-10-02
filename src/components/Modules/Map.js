2e3aa99dbd84e49d81867d68e49758ebf8d1f23c
Matt Watson <mattwat@gmail.com>
2019-10-02 09:57:38 -0400

    import necessary parts of react-simple-maps

--- src/components/Modules/Map.js
+++ src/components/Modules/Map.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Module } from '../Layout'
 import fabricMapSvg from '../../images/fabric-map.svg'
-
-
+import {
+    ComposableMap,
+    ZoomableGroup,
+    Geographies,
+    Geography,
+} from 'react-simple-maps'
 
 export const MapModule = props => {
     return (
